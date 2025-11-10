3d01595bf97b089e8f6d8e57817794c7f8d446db
puntig220112 <clemens.puntigam@gmail.com>
2025-11-10 08:01:28 +0100

    fertig

--- A1_ZweiThreadsZaehlenWinner/Program.cs
+++ A1_ZweiThreadsZaehlenWinner/Program.cs
@@ -10,17 +10,30 @@ class Program
     public static void Main(string[] args)
     {
         Console.WriteLine("Übung 1: Zwei Threads – Zählen & Winner");
-        
+        Thread threadA = new Thread(CountUpThreadA);
+        Thread threadB = new Thread(CountDownThreadB);
+        threadA.Start();
+        threadB.Start();
+        threadA.Join();
+        threadB.Join();
         
     }
     
     private static void CountUpThreadA()
     {
-        
+        for (int i = 1; i <= 100; i++)
+        {
+            Console.WriteLine($"Thread A: {i}");
+            Thread.Sleep(100); // Simuliere Arbeit
+        }
     }
     
     private static void CountDownThreadB()
     {
-       
+       for (int i = 100; i >= 1; i--)
+        {
+            Console.WriteLine($"Thread B: {i}");
+            Thread.Sleep(100); // Simuliere Arbeit
+        }
     }
 }
